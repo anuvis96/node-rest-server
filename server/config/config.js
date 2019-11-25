fceeb323bb827483b7e1b7887ed28e848734e36b
daniel <daniel.cano@aossas.com>
2019-11-25 14:46:05 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe-udemy';
 } else {
-    urlDB = 'mongodb+srv://root:nae8iUisVFPNoYDt@cluster0-ibrfv.mongodb.net/cafe-udemy';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
